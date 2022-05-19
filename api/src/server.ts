4e2fd26775b2664d08d35a6db9d5b605f269cf44
Alexandre Maeda <ale.b.maeda@gmail.com>
2022-05-18 21:18:03 -0300

    refactory

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import routes from './routes';
 import globallyErrors from './middlewares/globallyErrors';
